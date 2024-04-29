1e38c87e90dac343098613ff3d427df7b329a96f
Ryazanovskii <vlad.gorlach.2013@gmail.com>
2024-04-29 16:40:25 +0300

    first commit

--- Seminar1/main.go
+++ Seminar1/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func mergeSortedArrays(arr1 []int, arr2 []int) []int {
+/*func mergeSortedArrays(arr1 []int, arr2 []int) []int {
     mergedArray := make([]int, 0, len(arr1)+len(arr2))
     i, j := 0, 0
     for i < len(arr1) && j < len(arr2) {
@@ -24,4 +24,22 @@ func main() {
     arr2 := []int{2, 4, 6, 8, 9}
     merged := mergeSortedArrays(arr1, arr2)
     fmt.Println(merged) // Output: [1 2 3 4 5 6 7 8 9]
+}*/
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func main() {
+	arr := []int{6, 3, 9, 5, 2, 8}
+	fmt.Println("Before sorting:", arr)
+	bubbleSort(arr)
+	fmt.Println("After sorting:", arr)
 }
